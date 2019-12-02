4891e065ee0dfa9d6b17002ce27a7e3de4417869
patrickgue <me@patrickguenthard.ch>
2019-12-02 21:32:13 +0100

    Initial Commit

---
+++ SimpleNotes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SimpleNotes
+//
+//  Created by Patrick Günthard on 02.12.19.
+//  Copyright © 2019 Patrick Günthard. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SimpleNotes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SimpleNotes
+//
+//  Created by Patrick Günthard on 02.12.19.
+//  Copyright © 2019 Patrick Günthard. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
